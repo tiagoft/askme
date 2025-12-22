f7c9ce1c544162d0b16e67c9d81949eeb47f8f4d
Tiago Tavares <tiagoft@gmail.com>
2025-12-22 13:53:39 -0300

    wip: working on checking entailment with NLI

--- src/askme/askquestions/check_entailment.py
+++ src/askme/askquestions/check_entailment.py
@@ -1,16 +1,41 @@
 import torch
 
-def check_entailment_nli(model,
-                         tokenizer,
-                    premise: str,
-                    hypothesis: str,
-                    device='cuda:0',
-                    label_names = ['contradiction', 'entailment', 'neutral']
-                    ) -> tuple[bool, float, float, float]:
-    input = tokenizer(premise, hypothesis, truncation=True, return_tensors="pt")
-    output = model(input["input_ids"].to(device))  # device = "cuda:0" or "cpu"
-    prediction = torch.softmax(output["logits"][0], -1).tolist()
+
+def check_entailment_nli(
+    model,
+    tokenizer,
+    premise: str,
+    hypothesis: str,
+    device='cuda:0',
+    label_names=['contradiction', 'entailment', 'neutral']
+) -> tuple[bool, float, float, float]:
+    tokens = tokenizer(premise,
+                       hypothesis,
+                       truncation=True,
+                       return_tensors="pt")
+    output = model(
+        tokens["input_ids"].to(device))  # device = "cuda:0" or "cpu"
+    logits = output["logits"][0]
+
+    named_logits = {
+        name: round(float(logit), 1)
+        for logit, name in zip(logits, label_names)
     
-    prediction = {name: round(float(pred) * 100, 1) for pred, name in zip(prediction, label_names)}
-    print(prediction)
-    return (prediction["entailment"] > prediction["contradiction"]), prediction["entailment"], prediction["contradiction"]
+    }
+    
+    # P_entailment = P(entailment | entailment or contradiction)
+    # We ignore "neutral" for this probability calculation
+    # Following: https://arxiv.org/pdf/2303.08896
+    # (self-check GPT)
+    P_entailment = torch.softmax(
+        logits[[
+            label_names.index("entailment"),
+            label_names.index("contradiction")
+        ]], -1)[0].item()
+    
+    return (
+        (named_logits["entailment"] > named_logits["contradiction"]),
+        named_logits["entailment"],
+        named_logits["contradiction"],
+        P_entailment,
+    )
---
+++ tests/test_check_entailment_nli.py
@@ -0,0 +1,24 @@
+from askme.askquestions import check_entailment, models
+import pytest
+import transformers
+
+#     check_entailment_nli(
+#     model,
+#     tokenizer,
+#     premise: str,
+#     hypothesis: str,
+#     device='cuda:0',
+#     label_names=['contradiction', 'entailment', 'neutral']
+# )
+
+def test_check_entailment_nli():
+    model_name = 'MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7'
+    model, tokenizer = models.make_nli_model(model_name=model_name)
+    check_entailment.check_entailment_nli(
+        model,
+        tokenizer,
+        premise="The sky is blue.",
+        hypothesis="The sky is clear.",
+        device='cpu'
+    )
+    
